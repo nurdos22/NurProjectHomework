cb825f17eb70deb988e221d941a0937cd924c9d1
nurdos22 <nurdoskubatbekov@gmail.com>
2025-02-08 23:42:42 +0600

    homework 2.1

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 import asyncio
-import random
+from bot_config import dp, types
+from handler import start, pic, my_info, random
 
-from aiogram import Bot, Dispatcher
-from dotenv import dotenv_values
-
-token = dotenv_values(".env")["BOT_TOKEN"]
-bot = Bot(token=token)
-dp = Dispatcher(bot)
-
-random_user = ['Artur', 'Amir', 'Janat', 'Kairat']
-
-@dp.message_handler(commands =['start'])
-async def start_handler(message):
-    user = message.from_user
-    await message.answer(f'hello, {user.first_name}')
-
-@dp.message_handler(commands =['myinfo'])
-async def info_handler(message):
-    user = message.from_user
-    await message.answer(f'Your id: {message.from_user.id}\n'
-                         f' Your First name: {message.from_user.first_name}\n'
-                         f'Your username: {message.from_user.username} ')
-
-
-
-@dp.message_handler(commands =['random'])
-async def random_handler(message):
-    random_name = random.choice(random_user)
-    await message.answer(f" Случайное имя: {random_name}")
-
+async def echo_handler(message: types.message):
+    text = message.text
+    await message.answer(text)
 
 async def main():
+    start.register_handlers(dp)
+    pic.pic_handlers(dp)
+    my_info.register_handlers(dp)
+    random.register_handlers(dp)
+
     await dp.start_polling()
 
 if __name__ == '__main__':
